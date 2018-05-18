37637b39d5bad8a4b958f95d1d4f704f82d385dc
Sarah Woertler <sarah.woertler@gmail.com>
2018-05-18 11:17:40 +0200

    Task number 1

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,36 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    respond_to do |format|
+      if @cocktail.save
+        format.html { redirect_to @cocktail, notice: 'Cocktail was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+end
